a3212106053553785231c65054fbaf8c16cfe242
System Administrator <root@macs-MacBook-Pro-6.local>
2025-05-19 16:18:54 +0500

    First commit

---
+++ test/adify-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AdifyApi from '../lib/adify-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/adify-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AdifyApi.AdifyApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
